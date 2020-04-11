bfce63406f37ba980757a5a0edfe2738c13619a2
Anh Thu Tran <thuy.tran@adaptis.io>
2020-04-11 21:01:54 +0700

    styling page create supplier

--- resources/views/supplier/create.blade.php
+++ resources/views/supplier/create.blade.php
@@ -7,17 +7,19 @@
 @section('sidebar', '')
 
 @section('content')
-  <div class="col-md-8 offset-md-2">
-    <form action="{{ route('supplier.create') }}" method="POST" enctype="multipart/form-data">
+  <div class="col-md-8 offset-md-2 form-wrapper">
+    <h2 class="title text-center">Tạo tài khoản shop</h3>
+    <form action="{{ route('supplier.create') }}" method="POST" enctype="multipart/form-data" class="form">
       @csrf
       {{-- Hidden input --}}
       <input type="file" name="shopBanner" id="shopBanner" class="d-none" accept="image/*">
 
       <label for="shopBanner">Ảnh bìa: </label>
       <br>
-      <button class="btn btn-primary btn-image" type="button">Chọn ảnh</button>
-      <br>
-      <p class="image-name"></p>
+      <div class="input-file">
+        <button class="btn primary-btn primary-btn--square btn-image" type="button">Chọn ảnh</button>
+        <p class="image-name"></p>
+      </div>
       <img class="shop-banner img-responsive d-none" src="" alt="shop banner">
       <br>
       <label for="shop_name">Tên shop</label>
