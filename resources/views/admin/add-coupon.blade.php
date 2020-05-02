210bbbe24ae4411a9802078c55ce2c9d35b2c927
Anh Thu Tran <thuy.tran@adaptis.io>
2020-05-02 14:39:23 +0700

    add page add coupon of admin

--- resources/views/admin/add-coupon.blade.php
+++ resources/views/admin/add-coupon.blade.php
@@ -5,39 +5,60 @@
 @section('classname', 'add-coupon')
 
 @section('content')
-<form action="{{ route('admin.add-coupon') }}" method="POST" id="addCouponForm" class="form add-coupon-form">
-  @csrf
+<section class="section">
+  <h3>Tạo mã giảm giá</h3>
+  <form action="{{ route('admin.add-coupon') }}" method="POST" id="addCouponForm" class="form add-coupon-form">
+    @csrf
 
-  {{-- Input mã --}}
-  <br>
-  mã<input type="text" name="coupon-code" id="coupon-code">
+    {{-- Input mã --}}
+    <br>
+    <label for="coupon-code">
+      <strong>Code giảm giá:</strong>
+    </label>
+    <input type="text" name="coupon-code" id="coupon-code">
 
-  {{-- Input phần trăm --}}
-  <br>
-  phần trăm<input type="number" name="percent" id="percent">
+    {{-- Input phần trăm --}}
+    <br>
+    <label for="percent">
+      <strong>Phần trăm giảm:</strong>
+    </label>
+    <input type="number" name="percent" id="percent">
 
-  {{-- Input giảm tối đa bao nhiêu tiền --}}
-  <br>
-  giảm tối đa bao nhiêu tiền<input type="text" name="coupon-money" id="coupon-money" value="50000">
+    {{-- Input giảm tối đa bao nhiêu tiền --}}
+    <br>
+    <label for="coupon-money">
+      <strong>Phần trăm giảm:</strong>
+    </label>
+    <input type="text" name="coupon-money" id="coupon-money" value="50000">
 
-  {{-- Ngày bắt đầu --}}
-  <br>
-  Ngày bắt đầu<input type="date" name="start" id="start">
-
-
-  {{-- Ngày kết thúc --}}
-  <br>
-  Ngày kết thúc<input type="date" name="end" id="end">
-
-  {{-- Số lượng mã tối đa --}}
-  <br>
-  Số lượng mã tối đa<input type="number" name="number" id="number">
+    {{-- Ngày bắt đầu --}}
+    <br>
+    <label for="start">
+      <strong>Ngày bắt đầu có hiệu lực:</strong>
+    </label>
+    <input type="date" name="start" id="start">
 
 
-  <br>
+    {{-- Ngày kết thúc --}}
+    <br>
+    <label for="end">
+      <strong>Ngày kết thúc:</strong>
+    </label>
+    <input type="date" name="end" id="end">
 
-  {{-- Submit form --}}
-  <br>
-  <input type="submit" class="btn primary-btn primary-btn--square" value="Tạo mã giảm giá">
-</form>
+    {{-- Số lượng mã tối đa --}}
+    <br>
+    <label for="end">
+      <strong>Số lượng mã tối đa:</strong>
+    </label>
+    <input type="number" name="number" id="number">
+
+    <br>
+
+    {{-- Submit form --}}
+    <br>
+    <input type="submit" class="btn primary-btn primary-btn--square" value="Tạo mã giảm giá">
+  </form>
+</section>
+
 @endsection
